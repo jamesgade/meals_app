f98988ff7e2963b5307923ce0dc6a7161d307b94
jamesgade <gadejames48@gmail.com>
2023-07-31 12:03:32 +0530

    fine tuing 1 - using offset

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -81,8 +81,13 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(top: 100 - _animationController.value * 100),
+      builder: (context, child) => SlideTransition(
+        position: _animationController.drive(
+          Tween(
+            begin: const Offset(0, 0.4),
+            end: const Offset(0, 0),
+          ),
+        ),
         child: child,
       ),
     );
